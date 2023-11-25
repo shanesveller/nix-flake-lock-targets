85bd6d4f30c377b2d8f174670184df506bc16590
Shane Sveller <shanesveller@gmail.com>
2023-11-24 18:09:35 -0600

    Update nixpkgs inputs to 23.11

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Empty flake to align multiple repos' nixpkgs inputs";
 
   inputs = {
-    darwin.url = "nixpkgs/nixpkgs-23.05-darwin";
-    stable.url = "nixpkgs/nixos-23.05";
+    darwin.url = "nixpkgs/nixpkgs-23.11-darwin";
+    stable.url = "nixpkgs/nixos-23.11";
     unstable.url = "nixpkgs/nixos-unstable";
   };
 
